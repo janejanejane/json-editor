f6d5682fd03b3894473fefaf584edc6157f52c1b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2018-03-22 07:25:54 +1300

    Add disclaimer note

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -57,7 +57,11 @@ class Main extends React.Component {
                         </div>
                     </div>
                 </section>
-                <br />
+                <div class="notification is-warning">
+                    <strong>NOTE:</strong> 
+                    <p>No data is collected. All processing is done on the client browser using the WebAPI <a href="https://developer.mozilla.org/en-US/docs/Web/API/FileReader">FileReader</a>.</p>
+                    <p>The download uses the HTML5 saveAs method thru the <a href="https://github.com/eligrey/FileSaver.js/">FileSaver</a> library.</p>
+                </div>
                 <div class="container">
                     <div class="columns">
                         <div class="column is-half">
